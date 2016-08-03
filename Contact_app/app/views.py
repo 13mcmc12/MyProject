418b10e22c701c43fd26d074ac495e19e7c1dfc5
sanjay <sanjayhcu01@gmail.com>
2016-08-03 14:12:54 +0530

    Main project files

---
+++ Contact_app/app/views.py
@@ -0,0 +1,176 @@
+from app import app
+import json
+import os
+from contacts import Contact, ContactDetails, Contacts
+from flask import Flask, render_template, request
+contact_list = {}
+contacts = Contacts(contact_list)
+
+
+@app.route('/')
+def load_contacts_from_file():
+    if os.path.isfile('contacts_data.json') and os.path.getsize('contacts_data.json') > 0:
+        with open('contacts_data.json') as data_file:
+            json_data = json.load(data_file)
+        for key, value in json_data.items():
+            ct = Contact(value[ContactDetails.NAME], value[ContactDetails.PHONE_NO], value[ContactDetails.EMAIL],
+                         value[ContactDetails.ADDRESS][ContactDetails.STREET],
+                         value[ContactDetails.ADDRESS][ContactDetails.CITY],
+                         value[ContactDetails.ADDRESS][ContactDetails.STATE],
+                         value[ContactDetails.ADDRESS][ContactDetails.PIN_CODE])
+            contact_list[key] = ct
+    return render_template('contacts_home.html')
+
+@app.route('/contacts_home')
+def contacts_home():
+    return render_template('contacts_home.html')
+
+
+@app.route('/add_contact_form')
+def add_contact_form():
+    message = ''
+    return render_template('add_contact.html', message=message)
+
+
+@app.route('/add_contact', methods=['POST', 'GET'])
+def add_contact():
+    if request.method == 'POST':
+        name = request.form['name']
+        phone_no = request.form['phone_no']
+        email = request.form['email']
+        street = request.form['street']
+        city = request.form['city']
+        state = request.form['state']
+        pin_code = request.form['pin_code']
+        is_contact_exist = contacts.is_contact_exist(phone_no)
+        is_valid_phone_no = contacts.is_valid_phone_no(phone_no)
+        if (not is_contact_exist) and is_valid_phone_no:
+            contact = Contact(name, phone_no, email, street, city, state, pin_code)
+            contacts.add_contact(contact)
+            message = 'Contact successfully added!'
+            return render_template('add_contact.html', message=message)
+        elif is_contact_exist:
+            message = is_contact_exist
+            return render_template('add_contact.html', message=message)
+        else:
+            message = 'Invalid phone number!'
+            return render_template('add_contact.html', message=message)
+
+
+@app.route('/modify_contact_form')
+def modify_contact_form():
+    message = ''
+    return render_template('modify_contact.html', message=message)
+
+
+@app.route('/modify_contact', methods=['POST', 'GET'])
+def modify_contact():
+    if request.method == 'POST':
+        phone_no = request.form['phone_no']
+        is_contact_exist = contacts.is_contact_exist(phone_no)
+        if is_contact_exist:
+            fields = request.form
+            contacts.modify_contact(phone_no, fields)
+            message = 'Contact successfully modified!'
+            return render_template('modify_contact.html', message=message)
+        else:
+            message = 'There is no contact with this phone number!'
+            return render_template('modify_contact.html', message=message)
+
+
+@app.route('/delete_contact_form')
+def delete_contact_form():
+    message = ''
+    return render_template('delete_contact.html', message=message)
+
+
+@app.route('/delete_contact', methods=['POST', 'GET'])
+def delete_contact():
+    if request.method == 'POST':
+        phone_no = request.form['phone_no']
+        is_contact_exist = contacts.is_contact_exist(phone_no)
+        if is_contact_exist:
+            contacts.delete_contact(phone_no)
+            message = 'Contact deleted successfully!'
+            return render_template('delete_contact.html', message=message)
+        else:
+            message = 'There is no contact with this phone number!'
+            return render_template('delete_contact.html', message=message)
+
+
+@app.route('/get_contact_form')
+def get_contact_form():
+    message = ''
+    return render_template('get_contact.html', message=message)
+
+
+@app.route('/get_contact', methods=['POST', 'GET'])
+def get_contact():
+    if request.method == 'POST':
+        phone_no = request.form['phone_no']
+        is_contact_exist = contacts.is_contact_exist(phone_no)
+        if is_contact_exist:
+            details = contacts.get_contact(phone_no)
+            return render_template('view_contact.html', contact=details)
+        else:
+            message = 'There is no contact with this phone number!'
+            return render_template('get_contact.html', message=message)
+
+
+@app.route('/get_provider_form')
+def get_provider_form():
+    message = ''
+    return render_template('get_provider.html', message=message)
+
+
+@app.route('/get_provider', methods=['POST', 'GET'])
+def get_provider():
+    if request.method == 'POST':
+        phone_no = request.form['phone_no']
+        is_valid_phone_no = contacts.is_valid_phone_no(phone_no)
+        if is_valid_phone_no:
+            provider_name = contacts.get_provider(phone_no[0:4])
+            return render_template('get_provider.html', message=provider_name)
+        else:
+            message = 'Invalid phone number!'
+            return render_template('get_provider.html', message=message)
+
+
+@app.route('/get_contacts_by_provider_form')
+def get_contacts_by_provider_form():
+    return render_template('get_contacts_by_provider.html')
+
+
+@app.route('/get_contacts_by_provider', methods=['POST', 'GET'])
+def get_contacts_by_provider():
+    if request.method == 'POST':
+        provider_name = request.form['provider']
+        records = contacts.get_contacts_by_provider(provider_name)
+        return render_template('view_contacts.html', contacts=records)
+
+
+@app.route('/get_contacts_by_field_form')
+def get_contacts_by_field_form():
+    return render_template('get_contacts_by_field.html')
+
+
+@app.route('/get_contacts_by_field', methods=['POST', 'GET'])
+def get_contacts_by_field():
+    if request.method == 'POST':
+        string = request.form['string']
+        field = request.form['field']
+        li = contacts.get_contacts_by_field(string, field)
+        return render_template('view_contacts.html', contacts=li)
+
+
+@app.route('/save_contacts_to_file')
+def save_contacts_to_file():
+    final_contact_list = {}
+    for key, value in contacts.contact_list.items():
+        final_contact_list[key] = value.get_json()
+        final_contact_list[key][ContactDetails.ADDRESS] = final_contact_list[key][ContactDetails.ADDRESS].__dict__
+    with open('contacts_data.json', 'w') as data_file:
+        json.dump(final_contact_list, data_file, indent=2)
+    return render_template('exit.html')
+
+
